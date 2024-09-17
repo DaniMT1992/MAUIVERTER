4b5d4092956700cf88403c43c703b3efd680f5e8
Daniel Mosquer Taborda <danielmosquera@technium.dev>
2024-09-17 15:29:44 -0500

    Add project files.

---
+++ MAUIVERTER/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUIVERTER
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
